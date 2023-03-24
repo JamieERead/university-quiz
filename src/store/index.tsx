9a6a9801911cb2f8da33cb9e00f8d65eb4e65d1a
Jamie Read <james.read@sideshowagency.com>
2023-03-24 13:48:34 +0000

    chore: :sparkles: initial setup of project

---
+++ src/store/index.tsx
@@ -0,0 +1,38 @@
+import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";
+
+const reducers = combineReducers({});
+
+const presistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(presistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
